03899cc3392d540e7c9e10963f037811f8dca409
Muneeb <muhammadmuneebulraza@gmail.com>
2024-09-03 12:37:47 -0700

    make cart

---
+++ src/app/Component/Cart.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import CartItem from './CartItem';
+
+const cartData = [
+  {
+    title: "Title 1",
+    heading: "Heading 1",
+    content: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    imageUrl: "/my.jpeg",
+  },
+  {
+    title: "Title 2",
+    heading: "Heading 2",
+    content: "Consectetur possimus perspiciatis voluptates similique voluptas deleniti debitis.",
+    imageUrl: "/my.jpeg",
+  },
+  {
+    title: "Title 3",
+    heading: "Heading 3",
+    content: "Id rem blanditiis quasi rerum at ab, consectetur eos.",
+    imageUrl: "/my.jpeg",
+  },
+  {
+    title: "Title 4",
+    heading: "Heading 4",
+    content: "Id rem blanditiis quasi rerum at ab, consectetur eos.",
+    imageUrl: "/my.jpeg",
+  },
+];
+
+
+
+const Cart = () => {
+  return (
+    <div className="flex justify-center items-center min-h-screen">
+    <div className="grid grid-cols-1 sm:grid-cols-2  md:grid-cols-3 lg:grid-cols-4 gap-2">
+      {cartData.map((item, index) => (
+        <CartItem key={index} item={item} />
+      ))}
+    </div>
+  </div>
+  )
+}
+
+export default Cart
---
+++ src/app/Component/CartItem.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+
+interface CartItemProps {
+  item: {
+    title: string;
+    heading: string;
+    content: string;
+    imageUrl: string;
+  };
+}
+
+
+const CartItem = ({item}:CartItemProps) => {
+  return (
+    <div className="p-3 group">
+    <div className="relative w-[276px] h-[424px] overflow-hidden transition-all duration-300 border-2 border-transparent group-hover:border-purple-500 p-2">
+      <div className="w-full h-full p-2 bg-white">
+        <div
+          className="absolute inset-0 bg-cover bg-center transition-transform duration-500 ease-in-out group-hover:scale-150 group-hover:blur-sm"
+          style={{ backgroundImage: `url(${item.imageUrl})` }}
+        ></div>
+
+        <div className="relative w-full h-full p-4 z-10">
+          <div className="absolute top-3 left-3 text-white font-bold z-20">
+            {item.title}
+          </div>
+
+          <div className="absolute top-12 left-3 text-white font-bold text-xl z-20">
+            {item.heading}
+          </div>
+
+          <div className="absolute top-24 left-3 right-3 text-white transform translate-x-full opacity-0 transition-all duration-500 ease-in-out group-hover:translate-x-0 group-hover:opacity-100 z-20">
+            {item.content}
+          </div>
+
+          <div className="absolute bottom-3 right-3 transform translate-y-5 opacity-0 transition-all duration-500 ease-in-out group-hover:translate-y-0 group-hover:opacity-100 z-20">
+            <button className="text-white px-4 py-2">
+              Expand
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  )
+}
+
+export default CartItem
